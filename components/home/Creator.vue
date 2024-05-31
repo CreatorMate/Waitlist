73aa88220ba5343dc84b71bc892e75dd0297fb27
Robin Mons <robin.mons@hotmail.nl>
2024-05-31 11:24:56 +0200

    increased creator image size

--- components/home/Creator.vue
+++ components/home/Creator.vue
@@ -8,7 +8,7 @@
 
 <template>
     <div class="flex flex-col text-center font-medium">
-        <img :alt="`Creatormate friend ${creator.name}`" class="min-h-[220px] min-w-[220px]  xs:min-w-[280px] xs:min-h-[280px] md:min-w-[300px] md:min-h-[300px] 2xl:min-h-[360px] 2xl:min-w-[360px] 4xl:min-h-[400px] 4xl:min-w-[400px] 5xl:min-h-[600px] 5xl:min-w-[600px] 6xl:min-h-[700px] 6xl:min-w-[700px] object-cover rounded-lg" :src="`/creators/${creator.name}.png`">
+        <img :alt="`Creatormate friend ${creator.name}`" class="min-h-[220px] min-w-[220px]  xs:min-w-[280px] xs:min-h-[280px] md:min-w-[300px] md:min-h-[300px] xl:min-h-[360px] xl:min-w-[360px] 4xl:min-h-[400px] 4xl:min-w-[400px] 5xl:min-h-[600px] 5xl:min-w-[600px] 6xl:min-h-[700px] 6xl:min-w-[700px] object-cover rounded-lg" :src="`/creators/${creator.name}.png`">
         <div class="p-1 flex flex-col py-2">
             <h3 class="text-xl md:text-2xl">{{creator.name}}</h3>
             <p class="text-gray-400">{{creator.followers}} followers</p>
