78657ed7d021889611394cb60894d992af93a9bf
Robin Mons <robin.mons@hotmail.nl>
2024-05-31 18:04:12 +0200

    added profile icon wrapper

--- components/ProfileIcon.vue
+++ components/ProfileIcon.vue
@@ -16,7 +16,7 @@
     <router-link to="/profile" :class="[isActive ? 'bg-opacity-20' : 'bg-opacity-10']" class="flex gap-1.5 bg-white  py-0.5 pl-1.5 pr-3 rounded-full items-center cursor-pointer">
         <img alt="profile picture" class="w-6 h-6 object-fill rounded-full" :src="'https://jrirqcnnxpbhvmuugxnl.supabase.co/storage/v1/object/public/avatars/'+accountStore.avatar_url">
         <div class="py-0.5">
-            <p class="leading-6">{{accountStore.username}}</p>
+            <p class="leading-6 whitespace-nowrap">{{accountStore.username}}</p>
         </div>
     </router-link>
 </template>
